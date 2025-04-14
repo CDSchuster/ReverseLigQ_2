a133802f32ea94d711ec9542a464f2a2ea4416ed
CDSchuster <claudio.schuster.93@gmail.com>
2025-04-14 11:54:16 -0300

    Added comments to intersection_data function

--- generate_db/generate_db.py
+++ generate_db/generate_db.py
@@ -8,21 +8,41 @@ from generate_db.Interactions import get_interaction_data
 
 
 def intersect_data(pfam_data, interactions_data):
-    pfam_ids = []
-    pfam_names = []
+    """
+    Checks if the residue with which a ligand interacts is in some Pfam domain,
+    and then maps the Pfam domain to the interaction
 
+    Parameters
+    ----------
+    pfam_data : dataframe
+        Contains Pfam data for every chain in every PDB ID
+    interactions_data : dataframe
+        Ligand-protein interaction data retrieved from PDB
+
+    Returns
+    -------
+    interactions_data : dataframe
+        An updated version of interactions_data that includes the Pfam domain data
+    """
+
+    pfam_ids, pfam_names = [], [] # In these 2 lists we save the Pfam data to add to interaction_data
+    
     for row in interactions_data.itertuples(index=False):
+        # Iterate interactions_data rows and keep pdb_id, residue chain and residue number
         pdb_id, res_chain_id, resnum = row.pdb_id, row.res_chain_id, row.resnum
+        # For every combination of pdb_id and chain, we check pfam data in that chain
         matching_rows = pfam_data[(pfam_data['pdb_id'] == pdb_id) & (pfam_data['chain_id'] == res_chain_id)]
         
         found = False
+        # We iterate the pfam_data rows that have that PDB chain
         for pfrow in matching_rows.itertuples(index=False):
+            # Check if the residue with which the ligand interacts is in the Pfam domain
             if pfrow.start < resnum < pfrow.end:
                 pfam_ids.append(pfrow.pfam_id)
                 pfam_names.append(pfrow.pfam_name)
                 found = True
                 break  # Stop after first match
-
+        # If there is a match, we keep the Pfam data, otherwise we will put a NA
         if not found:
             pfam_ids.append(pd.NA)
             pfam_names.append(pd.NA)
@@ -50,13 +70,10 @@ def main():
     # Step 4: Retrieve interaction data
     results_dict["interactions_df"] = get_interaction_data(results_dict["ligand_df"])
 
+    # Step 5: Intersect interactions data and Pfam data
     final_df = intersect_data(results_dict["pfam_df"], results_dict["interactions_df"])
     final_df.to_csv("final_df.csv")
 
-    #results_dict["ligand_df"].to_csv("ligand.csv", index=False)
-    #results_dict["pfam_df"].to_csv("pfam.csv", index=False)
-    #results_dict["interactions_df"].to_csv("interactions.csv", index=False)
-    
     return results_dict
 
 

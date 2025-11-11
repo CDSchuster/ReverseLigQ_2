b8eb6879db5e7d8a59d7cdf1dece9785c8b29c64
JHK212 <153141816+JHK212@users.noreply.github.com>
2025-11-11 16:01:36 -0300

    Update generate_db.py

--- db_generation/generate_db.py
+++ db_generation/generate_db.py
@@ -7,6 +7,6 @@ log = logging.getLogger("generateDB_log")
 
 
 def run_database_generation(min_actives, max_actives, threshold, min_decoys, max_decoys):
-    #generate_pdb_db.main()
+    generate_pdb_db.main()
     log.info("Starting PDB smiles pairs dataset generation")
     pdb_smiles_dataset = generate_smiles_pairs_dataset(min_actives, max_actives, threshold, min_decoys, max_decoys)
